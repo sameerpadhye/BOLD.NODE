7f236aef9199746a6d2a934565cf013dcae21bde
Sameer Padhye <sameer.m.padhye@gmail.com>
2026-04-22 15:37:24 -0400

    occurrence data function update
    updated the function to handle non Animalia organisms. Also updated the details for user manual

--- R/external.get.occ.data.R
+++ R/external.get.occ.data.R
@@ -2,7 +2,8 @@
 #'
 #' @description Extracts occurrence data (specimen counts by taxon and location) from BOLD search results.
 #'
-#' @details This function transforms the search results from `bold.data.search` into occurrence data matrices used commonly in biodiversity and ecological analyses by packages like `vegan` and `betapart`. It supports aggregation at different taxonomic ranks (from kingdom to species), optional filtering by specific taxon names, and grouping by site categories or geographic coordinates. The function can convert count data to presence/absence (1/0) format.
+#' @details This function transforms the search results from `bold.data.search` into occurrence data matrices used commonly in biodiversity and ecological analyses by packages like `vegan` and `betapart`. `kingdom` argument specifies the taxa kingdom with the default value being `Animalia`. The occurrences differ based on the kingdom. For `Animalia`, only records having BINs are counted (i.e. records without BINs are removed before calculations), while for other kingdoms, all records having a sequence are counted (i.e. records without sequences are removed before calculations). It supports aggregation at different taxonomic ranks (from kingdom to species) for a single or multiple taxa (also applicable for bin_uri; the difference being that the numbers in each cell would be the number of times that respective BIN is found at a particular `site.cat`), optional filtering by specific taxon names. `site.cat` can be any of the `geography` fields (Meta data on fields can be checked using the `bold.bcdm.fields` function). In case where `site.cat` = NULL, the column `coord` will be used as default. The function can convert count data to presence/absence (1/0) format. In case `specific.cols` is used in the `bold.data.search` function to obtain only few columns, occurrence matrix wont be generated.
+#'
 #'
 #' @param bold.search.res A `tbl_sql` object containing BOLD search results
 #' @param taxon.rank Taxonomic rank to aggregate by (kingdom, phylum, class, order, family, genus, or species)
@@ -30,6 +31,7 @@
 #'
 #' occurrence_data <- get.occ.data(
 #'   bold_search,
+#'   kingdom = 'Animalia',
 #'   taxon.rank = "family",
 #'   site.cat = "region"
 #'   )
@@ -37,6 +39,7 @@
 #' }
 #' @export
 get.occ.data <- function(bold.search.res,
+                         kingdom="Animalia",
                          taxon.rank,
                          taxon.name = NULL,
                          site.cat = NULL,
@@ -46,14 +49,16 @@ get.occ.data <- function(bold.search.res,
   check.tbl.sql(bold.search.res)
 
   rank_map <- c(kingdom = "kingdom",
-    phylum = "phylum",
-    class   = "class",
-    order   = "order",
-    family  = "family",
-    genus   = "genus",
-    species = "species")
+                phylum = "phylum",
+                class   = "class",
+                order   = "order",
+                family  = "family",
+                genus   = "genus",
+                species = "species",
+                bin_uri = 'bin_uri')
 
   taxon.rank <- rank_map[[tolower(taxon.rank)]]
+
 
   if (is.null(taxon.rank)) {
     stop("Invalid taxonomic rank supplied.")
@@ -62,13 +67,36 @@ get.occ.data <- function(bold.search.res,
   cols_to_select <- Filter(Negate(is.null),
                            site.cat)
 
-  occ.data <- bold.search.res %>%
-    dplyr::select(
-      bin_uri = matches("bin_uri$", ignore.case = TRUE),
-      coord   = matches("coord$", ignore.case = TRUE),
-      taxon   = !!rlang::sym(taxon.rank),
-      dplyr::all_of(cols_to_select)) %>%
-    dplyr::filter(bin_uri != "None", !is.na(bin_uri))
+
+  if(kingdom=='Animalia')
+
+  {
+
+    occ.data <- bold.search.res %>%
+      dplyr::select(
+        bin_uri = matches("bin_uri$", ignore.case = TRUE),
+        coord   = matches("coord$", ignore.case = TRUE),
+        taxon   = !!rlang::sym(taxon.rank),
+        dplyr::all_of(cols_to_select)) %>%
+      dplyr::filter(!is.na(bin_uri),bin_uri!='')%>%
+      dplyr::filter(!is.na(taxon))
+
+
+
+  } else if (kingdom%in%c('Plantae','Protista','Fungi','Mixture', 'Bacteria'))
+
+  {
+
+    occ.data <- bold.search.res %>%
+      dplyr::select(
+        nuc = matches("nuc$", ignore.case = TRUE),
+        coord   = matches("coord$", ignore.case = TRUE),
+        taxon   = !!rlang::sym(taxon.rank),
+        dplyr::all_of(cols_to_select)) %>%
+      dplyr::filter(!is.na(nuc),nuc!='')%>%
+      dplyr::filter(!is.na(taxon))
+
+  }
 
 
   if (!is.null(taxon.name)) {
@@ -86,7 +114,7 @@ get.occ.data <- function(bold.search.res,
       dplyr::count(
         .data[[site.cat]],
         taxon,
-        name = "bin_count"
+        name = "count"
       )
 
   } else {
@@ -96,20 +124,22 @@ get.occ.data <- function(bold.search.res,
       dplyr::count(
         coord,
         taxon,
-        name = "bin_count"
+        name = "count"
       )
 
   }
 
 
-  # Collect AFTER aggregation (important for large datasets)
+  # Collect
   occ.data <- occ.data %>%
     dplyr::collect() %>%
     tidyr::pivot_wider(
       names_from  = taxon,
-      values_from = bin_count,
+      values_from = count,
       values_fill = 0
     )
 
-  occ.data
+  return(occ.data)
+
+
 }
